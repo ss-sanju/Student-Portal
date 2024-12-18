1206716093bfc9825e9cf6d74657cc4f2a37792c
ss-sanju <yajnasstha79@gmail.com>
2024-12-18 14:02:56 +0545

    first commit

---
+++ StudentPortal.Web/Controllers/StudentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentPortal.Web.Controllers
+{
+    public class StudentsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add()
+        {
+
+        }
+    }
+}
---
+++ StudentPortal.Web/Models/AddStudentViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentPortal.Web.Models
+{
+    public class AddStudentViewModel
+    {
+        public String Name { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool Subscribed { get; set; }
+    }
+}
